d0e403b6ced6708b8e8614793511e196666e11a7
ksanchic <ksanchic@mail.ru>
2022-12-11 13:48:31 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,26 @@
+﻿//программа, которая задает массив из 8 элементов и выводит их на экран
+
+
+{
+int[] array = new int[8];
+Console.WriteLine($"Введите элементы массива:");
+
+    for (int i =0; i< array.Length; ++i)
+        {
+         Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        array[i] = int.Parse(Console.ReadLine());
+        }
+
+    Console.WriteLine("\nВывод массива:");
+
+    for (int i =0; i< array.Length; ++i)
+        {
+        Console.Write(array[i]);
+        Console.Write(", ");
+    
+        }
+
+    Console.ReadLine();
+
+}
+
