296646e7d770b42366e3c7734c4d3e2e08de7f04
ksanchic <ksanchic@mail.ru>
2022-12-11 13:26:17 +0300

    task 25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную 
+//степень В(без использования Math.Pow)
+
+
+Console.WriteLine ("Введите число А");
+Console.WriteLine("Введите степень n:");
+{
+int A = int.Parse (Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+ int s=1;
+
+if (n <1)
+ {
+    Console.WriteLine("некорректное число");
+ } 
+else
+{
+  for (int i=0; i<n; i++)
+       s*=A;
+       Console.WriteLine("{0} ^ {1} = {2}", A, n, s);
+       Console.ReadKey();
+}
+}
